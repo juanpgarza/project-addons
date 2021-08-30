d7f4400c04eb6db34405728657d2dc1ac19b1c4f
Juan Pablo Garza <juanp@juanpgarza.com>
2021-08-30 18:38:13 -0300

    [14.0][ADD] project_address: creación inicial

---
+++ project_address/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Address",
+    "summary": "Project address",
+    "version": "14.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/juanpgarza/project-addons",
+    "author": "juanpgarza",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        ],
+    "data": [
+        "views/project_project_views.xml",
+        ],
+    "installable": True,
+    "maintainers": ["juanpgarza"],    
+}
---
+++ project_address/models/project_project.py
@@ -0,0 +1,10 @@
+# Copyright 2019 Patrick Wilson <patrickraymondwilson@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class Project(models.Model):
+    _inherit = "project.project"
+
+    partner_delivery_id = fields.Many2one(comodel_name="res.partner",string="Dirección de la Obra")
