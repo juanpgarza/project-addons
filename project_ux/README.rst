683a252c25921abf087176194cdb28d9eb885c99
Juan Pablo Garza <juanp@juanpgarza.com>
2021-09-03 17:53:55 -0300

    [14.0][ADD] project_ux: creación inicial

---
+++ project_ux/README.rst
@@ -0,0 +1,8 @@
+========
+Project UX
+========
+
+Mejoras varias a Proyectos:
+
+#. Que permita asignar tareas a usuarios del Portal (de fábrica solo permite usuarios internos)
+#. Que al clickear una tarjeta del kanban de proyectos navegue al proyecto (ahora navega a las tareas del proyecto) [TODO]
---
+++ project_ux/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project UX",
+    "summary": "Project ux",
+    "version": "14.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/juanpgarza/project-addons",
+    "author": "juanpgarza",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        ],
+    "data": [
+        "views/project_task_views.xml",
+        ],
+    "installable": True,
+    "maintainers": ["juanpgarza"],    
+}
