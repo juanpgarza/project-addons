cb23100d6b5a3cb5947c5bb051db6cb9953f9726
Juan Pablo Garza <juanp@juanpgarza.com>
2022-07-17 19:37:57 -0300

    [15.0][IMP] project_tsm_account: calcular precio del servicio segun proveedor

--- project_tsm_account/wizards/invoice_add_tsm_service.py
+++ project_tsm_account/wizards/invoice_add_tsm_service.py
@@ -12,7 +12,7 @@ class InvoiceAddTsmService(models.TransientModel):
 
     tsm_service_ids = fields.Many2many("project.tsm.service",
                                 string="Servicios",
-                                domain="[('partner_id','=',partner_id),('state','=','approved'),('subcontracted','=',True)]")
+                                domain="[('partner_id','=',partner_id),('state','=','approved'),('subcontracted','=',True),('qty_to_invoice','>',0)]")
 
     @api.model
     def default_get(self, field_names):
@@ -24,23 +24,44 @@ class InvoiceAddTsmService(models.TransientModel):
     def create_items(self):
         aml = self.env["account.move.line"]
         for service in self.tsm_service_ids:
-            vals_1 = {
-                'move_id': self.account_move_id.id,
-                'account_id': service.product_template_id.property_account_expense_id.id or service.product_template_id.categ_id.property_account_expense_categ_id.id,
-                'product_id': service.product_template_id.product_variant_id.id,
-                'product_uom_id': service.product_template_id.uom_id.id,                
-                'quantity': service.qty_delivered - service.qty_invoiced,
-                'tax_ids': [(6, 0, service.product_template_id.supplier_taxes_id.ids)],
-                'tsm_service_id': service.id,
-                # 'price_unit': service.product_template_id.standard_price,
-                # 'debit': service.product_template_id.standard_price,
-                # 'credit': 0,
-            }
-            aml.create(vals_1)
-            # vals_2 = vals_1
-            # vals_2["debit"] = 0
-            # vals_2["credit"] = service.product_template_id.standard_price
-            # aml.create([vals_1,vals_2])
+            product_id = service.product_template_id.product_variant_id
+            # src/addons/purchase/models/purchase.py: 1336
+            # de la po saco como obtener el precio unitario según el proveedor
+            # src/addons/product/models/product.py:637 _select_seller
+            # _select_seller is used if the supplier have different price depending
+            # the quantities ordered.            
+            # seller = product_id.with_company(company_id)._select_seller(
+            #     partner_id=partner,
+            #     quantity=uom_po_qty,
+            #     date=po.date_order and po.date_order.date(),
+            #     uom_id=product_id.uom_po_id)
+            move_id = self.account_move_id
+            seller = product_id.seller_ids.filtered(lambda x: x.name == move_id.partner_id)
 
-            
+            product_taxes = product_id.supplier_taxes_id.filtered(lambda x: x.company_id.id == move_id.company_id)
+            taxes = move_id.fiscal_position_id.map_tax(product_taxes)
+
+            price_unit = self.env['account.tax']._fix_tax_included_price_company(
+                seller.price, product_taxes, taxes, move_id.company_id) if seller else 0.0
+            if price_unit and seller and move_id.currency_id and seller.currency_id != move_id.currency_id:
+                price_unit = seller.currency_id._convert(
+                    price_unit, move_id.currency_id, move_id.company_id, move_id.date or fields.Date.today())
+
+            vals_1 = {
+                'product_id': product_id.id,
+                'product_uom_id': service.product_template_id.uom_id.id, 
+                'quantity': service.qty_delivered - service.qty_invoiced,
+                'price_unit': price_unit,
+                'tax_ids': [(6, 0, service.product_template_id.supplier_taxes_id.ids)],                                
+                'move_id': move_id.id,
+                'account_id': service.product_template_id.property_account_expense_id.id or service.product_template_id.categ_id.property_account_expense_categ_id.id,              
+                'tsm_service_id': service.id,
+            }
+            # src/addons/purchase/models/account_invoice.py:54
+            # necesario el check_move_validity=False (sino da error)
+            aml_new = aml.with_context(check_move_validity=False).create(vals_1)
+            #import pdb; pdb.set_trace()
+            aml_new._onchange_price_subtotal()
+            aml_new._onchange_mark_recompute_taxes()
+   
         
