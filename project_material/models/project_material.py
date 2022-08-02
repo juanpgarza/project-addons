30c4c945b899558cfc3304b94a466ce19ea607db
Juan Pablo Garza <juanp@juanpgarza.com>
2022-08-02 12:35:09 -0300

    [15.0][ADD] project_material

---
+++ project_material/models/project_material.py
@@ -0,0 +1,31 @@
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+
+class ProjectMaterial(models.Model):
+    _name = 'project.material'
+    _description = 'Materiales de la tarea'
+    _rec_name = "product_id"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+
+    project_id = fields.Many2one(
+        comodel_name="project.project", string="Proyecto", ondelete="cascade", required=True
+    )
+    task_id = fields.Many2one(
+        comodel_name="project.task", string="Tarea", ondelete="cascade", required=False, domain="[('project_id','=',project_id)]"
+    )    
+    product_id = fields.Many2one(
+        comodel_name="product.product", string="Producto", required=True
+    )
+    quantity = fields.Float("Cantidad")
+
+    @api.constrains("quantity")
+    def _check_quantity(self):
+        for material in self:
+            if not material.quantity > 0.0:
+                raise ValidationError(
+                    ("La cantidad de material debe ser mayor a 0.")
+                )
---
+++ project_material/wizards/task_material_import.py
@@ -0,0 +1,57 @@
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, Warning
+import base64
+from io import BytesIO
+from xlrd import open_workbook
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class TaskMaterialImport(models.TransientModel):
+    _name = 'task.material.import.wizard'
+    _description = 'Importación de productos'
+
+    project_id = fields.Many2one(comodel_name="project.project",
+                                string="Projecto")
+
+    excel_file_for_import = fields.Binary("Archivo")
+    file_name = fields.Char("File Name")
+
+    @api.model
+    def default_get(self, field_names):
+        defaults = super(
+            TaskMaterialImport, self).default_get(field_names)
+        defaults['project_id'] = self.env.context['active_id']
+        return defaults
+
+    def do_import(self):
+        try:
+            inputx = BytesIO()
+            inputx.write(base64.decodestring(self.excel_file_for_import))
+            book = open_workbook(file_contents=inputx.getvalue())
+        except TypeError as e:
+            raise UserError(u'ERROR: {}'.format(e))
+
+        sheet = book.sheets()[0]
+
+        material_tarea = self.env["project.material"]
+        for i in list(range(sheet.nrows)):
+            # columna 0 = código de producto
+            default_code = int(sheet.cell(i, 0).value)
+            # columna 1 = descripcion
+            # columna 2 = cantidad
+            cantidad = sheet.cell(i, 2).value
+
+            producto = self.env["product.template"].search([("default_code","=", default_code)])
+
+            val = {
+                # 'task_id': task_id.id,
+                'project_id': self.project_id.id,
+                'product_id': producto.product_variant_id.id,
+                'quantity': cantidad
+            }
+
+            material_tarea.create(val)            
