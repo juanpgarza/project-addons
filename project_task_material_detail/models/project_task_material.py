3af8e2aeed5f0ca47ac3dfa3cf3fab3c0fcf74b1
Juan Pablo Garza <juanp@juanpgarza.com>
2022-08-04 15:50:29 -0300

    [15.0][REM] project_task_material_detail,project_task_material_import

--- project_task_material_detail/models/project_task_material.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import _, api, exceptions, fields, models
-
-class ProjectTaskMaterial(models.Model):
-    _inherit = "project.task.material"
-
-    project_id = fields.Many2one(string='',related='task_id.project_id')
