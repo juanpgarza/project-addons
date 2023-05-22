b72d3529e09180fcb8e908b3c70350f98154b808
Juan Pablo Garza <juanp@juanpgarza.com>
2023-05-22 14:57:07 -0300

    [15.0][FIX] t59953 project_task_report_libra: hojas en blanco al final

--- project_task_report_libra/__manifest__.py
+++ project_task_report_libra/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "project_task_report_libra",
     "summary": "project_task_report_libra",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "category": "Project",
     "website": "https://github.com/juanpgarza/project-addons",
     "author": "juanpgarza",
