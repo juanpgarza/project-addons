c738d297eeb54144d6c971d02ac5e4e8557b80b7
Juan Pablo Garza <juanp@juanpgarza.com>
2025-08-25 16:08:47 -0300

    FIX project_task_checklist

--- project_task_checklist/models/project_task.py
+++ project_task_checklist/models/project_task.py
@@ -1,7 +1,4 @@
-# Copyright 2021 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import _, api, exceptions, fields, models
+from odoo import _, api, fields, models
 
 class ProjectTask(models.Model):
     _inherit = "project.task"
@@ -13,26 +10,42 @@ class ProjectTask(models.Model):
         copy=True
     )
 
-    def map_checks(self, new_task_id):
-        """ copy and map checks from old to new task """
-        task = self.browse(new_task_id)
+    def map_checks(self, new_task):
+        """Copy and map checks from old to new task"""
+        self.ensure_one()   # ⚠️ garantiza que sea de a una tarea
+
         checks = self.env['project.task.check']
-        # We want to copy archived task, but do not propagate an active_test context key
-        check_ids = self.env['project.task.check'].with_context(active_test=False).search([('task_id', '=', self.id)], order='sequence').ids
-        defaults = {}
-        for check in self.env['project.task.check'].browse(check_ids):
-            defaults['task_id'] = new_task_id
-            new_check = check.copy(defaults)
+        # queremos copiar también archivados, sin active_test
+        old_checks = self.env['project.task.check'].with_context(active_test=False).search(
+            [('task_id', '=', self.id)], order='sequence'
+        )
+
+        for check in old_checks:
+            new_check = check.copy({'task_id': new_task.id})
             checks += new_check
 
-        return task.write({'check_ids': [(6, 0, checks.ids)]})
+        return new_task.write({'check_ids': [(6, 0, checks.ids)]})
 
-    # src/addons/project/models/project.py:440
     @api.returns('self', lambda value: value.id)
     def copy(self, default=None):
-        task = super(ProjectTask, self).copy(default)
-        for follower in self.message_follower_ids:
-            task.message_subscribe(partner_ids=follower.partner_id.ids, subtype_ids=follower.subtype_ids.ids)
+        new_tasks = super(ProjectTask, self).copy(default)
 
-        self.map_checks(task.id)
-        return task
+        if len(self) == 1:
+            # caso singleton
+            for follower in self.message_follower_ids:
+                new_tasks.message_subscribe(
+                    partner_ids=follower.partner_id.ids,
+                    subtype_ids=follower.subtype_ids.ids
+                )
+            self.map_checks(new_tasks)
+        else:
+            # caso batch: varias tareas de golpe (ej: duplicar proyecto con muchas tareas)
+            for old_task, new_task in zip(self, new_tasks):
+                for follower in old_task.message_follower_ids:
+                    new_task.message_subscribe(
+                        partner_ids=follower.partner_id.ids,
+                        subtype_ids=follower.subtype_ids.ids
+                    )
+                old_task.map_checks(new_task)
+
+        return new_tasks
